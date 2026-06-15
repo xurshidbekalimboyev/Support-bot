375bad352b465b7ecd67f56c6573aab5cc9723ec
xurshidbekalimboyev <xurshidbekalimboyev@gmail.com>
2026-06-15 12:09:54 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
+import asyncio
+from aiogram import Bot, Dispatcher
+from config import 8840128817:AAHXz5o4v_HrwW9UJfa3Ne7C1Vnd1cwowc8
+import handlers
 
+bot = Bot(8840128817:AAHXz5o4v_HrwW9UJfa3Ne7C1Vnd1cwowc8)
+dp = Dispatcher()
+
+dp.include_router(handlers.router)
+
+async def main():
+    print("Bot ishga tushdi...")
+    await dp.start_polling(bot)
+
+if __name__ == "__main__":
+    asyncio.run(main())
